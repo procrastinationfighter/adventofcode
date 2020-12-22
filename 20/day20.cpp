7ee89b948dca92ad2e3612fdf0d5ab23c3d3f95b
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2020-12-22 15:46:26 +0100

    Day twenty-second.

---
+++ 20/day20.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+const int TILE_SIZE = 9;
+
+class Tile {
+ private:
+  std::vector<std::string> pattern;
+  Tile *left, *up, *down, *right;
+  const int id;
+
+ public:
+  Tile(std::vector<std::string> p, int id) : pattern(std::move(p)), id(id) {
+      left = right = up = down = nullptr;
+  }
+
+  bool try_appending_tile(Tile *tile) {
+      bool ok = false;
+
+      if (left == nullptr) {
+          ok = true;
+          for (int i = 0; i < TILE_SIZE && ok; i++) {
+              if (pattern[i][0] != tile->pattern[i][TILE_SIZE - 1]) {
+                  ok = false;
+              }
+          }
+
+          if (ok) {
+              left = tile;
+              tile->right = this;
+          }
+      }
+
+      if (right == nullptr && !ok) {
+          ok = true;
+          for (int i = 0; i < TILE_SIZE && ok; i++) {
+              if (pattern[i][TILE_SIZE - 1] != tile->pattern[i][0]) {
+                  ok = false;
+              }
+          }
+
+          if (ok) {
+              right = tile;
+              tile->left = this;
+          }
+      }
+
+      if (up == nullptr && !ok) {
+          ok = true;
+          for (int i = 0; i < TILE_SIZE && ok; i++) {
+              if (pattern[0][i] != tile->pattern[TILE_SIZE - 1][i]) {
+                  ok = false;
+              }
+          }
+
+          if (ok) {
+              up = tile;
+              tile->down = this;
+          }
+      }
+
+      if (down == nullptr && !ok) {
+          ok = true;
+          for (int i = 0; i < TILE_SIZE && ok; i++) {
+              if (pattern[TILE_SIZE - 1][i] != tile->pattern[0][i]) {
+                  ok = false;
+              }
+          }
+
+          if (ok) {
+              down = tile;
+              tile->up = this;
+          }
+      }
+
+      return ok;
+  }
+
+  // Rotates by 90 degrees
+  void rotate() {
+      std::vector<std::string> temp(TILE_SIZE, std::string(TILE_SIZE, '*'));
+      int N = temp.size();
+      for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++)
+				temp[i][j] = pattern[N - j - 1][i];
+		}
+      pattern = temp;
+  }
+
+  [[nodiscard]] int get_id() const {
+      return id;
+  }
+
+  [[nodiscard]] Tile* get_right() const {
+      return right;
+  }
+
+  [[nodiscard]] Tile* get_down() const {
+      return down;
+  }
+
+};
+
+class Image {
+ private:
+  Tile *left_upper = nullptr;
+
+ public:
+  Image() = default;
+
+  bool try_appending(Tile *tile) {
+      //todo BFS/DFS
+  }
+
+  long long get_corners_multiplication() {
+      long long mult = left_upper->get_id();
+      Tile *curr = left_upper;
+      Tile *prev;
+      while (curr != nullptr) {
+          prev = curr;
+          curr = prev->get_down();
+      }
+
+      mult *= prev->get_id();
+
+      curr = prev;
+      while (curr != nullptr) {
+          prev = curr;
+          curr = prev->get_right();
+      }
+
+      mult *= prev->get_id();
+
+      curr = left_upper;
+      prev = nullptr;
+      while (curr != nullptr) {
+          prev = curr;
+          curr = prev->get_right();
+      }
+
+      mult *= prev->get_id();
+
+      return mult;
+  }
+
+};
+
+int main() {
+
+    return 0;
+}
---
+++ 22/day22.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <set>
+
+std::vector<int> one_deck, second_deck;
+
+void init() {
+    std::string curr;
+    std::getline(std::cin, curr);
+    std::getline(std::cin, curr);
+    while(!curr.empty()) {
+        one_deck.push_back(std::stoi(curr));
+        std::getline(std::cin, curr);
+    }
+
+    std::getline(std::cin, curr);
+
+    while(std::getline(std::cin, curr)) {
+        second_deck.push_back(std::stoi(curr));
+    }
+
+}
+
+long part_one() {
+    std::deque<int> first, second;
+    for(int x : one_deck) {
+        first.push_back(x);
+    }
+
+    for(int x : second_deck) {
+        second.push_back(x);
+    }
+
+    while (!first.empty() && !second.empty()) {
+        int f = first.front(), s = second.front();
+        first.pop_front();
+        second.pop_front();
+
+        if(f > s) {
+            first.push_back(f);
+            first.push_back(s);
+        }
+        else {
+            second.push_back(s);
+            second.push_back(f);
+        }
+    }
+
+    std::deque<int> &winner = first.empty() ? second : first;
+
+    long sum = 0;
+    int i = 1;
+
+    while (!winner.empty()) {
+        sum += i * winner.back();
+        winner.pop_back();
+        i++;
+    }
+
+    return sum;
+}
+
+std::deque<int> clone_deck(std::deque<int> deck, int number_of_cards) {
+    while(deck.size() > number_of_cards) {
+        deck.pop_back();
+    }
+
+    return deck;
+}
+
+// True if player 1 won.
+bool recursive_combat(std::deque<int> &first, std::deque<int> &second) {
+    std::set<std::pair<std::deque<int>, std::deque<int>>> former_positions;
+
+    while (!first.empty() && !second.empty()) {
+        if (former_positions.find({first, second}) != former_positions.end()) {
+            return true;
+        } else {
+            former_positions.insert({first, second});
+            int f = first.front(), s = second.front();
+            first.pop_front();
+            second.pop_front();
+
+            if (f <= first.size() && s <= second.size()) {
+                auto first_copy = clone_deck(first, f);
+                auto second_copy = clone_deck(second, s);
+
+                if (recursive_combat(first_copy, second_copy)) {
+                    first.push_back(f);
+                    first.push_back(s);
+                } else {
+                    second.push_back(s);
+                    second.push_back(f);
+                }
+            } else {
+                if (f > s) {
+                    first.push_back(f);
+                    first.push_back(s);
+                } else {
+                    second.push_back(s);
+                    second.push_back(f);
+                }
+            }
+        }
+    }
+
+    return second.empty();
+}
+
+long part_two() {
+    std::deque<int> first, second;
+    for(int x : one_deck) {
+        first.push_back(x);
+    }
+
+    for(int x : second_deck) {
+        second.push_back(x);
+    }
+
+    std::deque<int> &winner = recursive_combat(first, second) ? first : second;
+
+    long sum = 0;
+    int i = 1;
+
+    while (!winner.empty()) {
+        sum += i * winner.back();
+        winner.pop_back();
+        i++;
+    }
+
+    return sum;
+}
+
+
+int main() {
+    init();
+    std::cout << part_one() << " " << part_two() << "\n";
+    return 0;
+}
